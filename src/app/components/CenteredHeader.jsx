d41f2969b86bdc286e666088ffae362ad671016c
Mark Benliyan <markbenliyan@g.ucla.edu>
2023-07-11 18:32:36 -0700

    initial setup

---
+++ src/app/components/CenteredHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CenteredHeader() {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">So, what do we do?</h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          We partner with students who are <b>serious about the LSAT</b> and <b>actively studying</b>. We host intimate prep sessions of up to 6 students led by an instructor who has personally scored in the 99th percentile. 
+        </p>
+      </div>
+    </div>
+  )
+}
